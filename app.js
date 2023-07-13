10d00db5e294ed6c067601fbeb69822cab088861
KhaledAbuarida <khaledabuarida0@gmail.com>
2023-07-14 00:37:20 +0300

    finishing the assignment

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+const users = [];
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.get('/users', (req, res, next) => {
-    res.render();
-});
+app.use(bodyParser({extended: false}));
 
-app.get('/', (req, res, next) => {
-    res.render();
+app.get('/users', (req, res, next) => {
+    res.render('users', {pageTitle: 'users', users: users, hasUsers: users.length > 0});
 });
 
 app.post('/add-user', (req, res, next) => {
+    users.push({userName: req.body.title})
     res.redirect('/users');
+});
+
+app.get('/', (req, res, next) => {
+    res.render('add-user', {pageTitle: 'Add User'});
 });
 
 app.listen(5000);
